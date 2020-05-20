42b89f8b9af5a3f3f52cf2da6d1963b0a55c288d
Manuel Bernacki <manuel.bernacki@gmail.com>
2020-05-20 13:37:26 +0200

    Added basic NavBar

--- public/index.php
+++ public/index.php
@@ -3,52 +3,28 @@ require_once('../init.php');
 ?>
 <!DOCTYPE html>
 <html lang="de">
-<head>
-    <title><?php echo Config('app','name'); ?></title>
-    <meta name="description" content="Bitte kaufen Sie unsere legitim erworbenen Fotografien.">
-    <meta name="keywords" content="FHTW, Technikum">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
-    <link rel="stylesheet" type=text/css href="js/jquery-ui-1.12.1/jquery-ui.min.css" />
+<?php
+include("../subpages/head.php");
+include("../subpages/navBar.php");
 
-    <link rel="stylesheet" type=text/css href="assets/BasicModal.css" />
-    <link rel="stylesheet" type=text/css href="assets/CustomShop.css" />
-    <link rel="shortcut icon" href="assets/fhtw-16x16.ico" >
-    <script src="js/index.js"></script>
-</head>
+?>
+
 <body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <main class="col-12">
+            <?php
+            $subpage = trim(filter_input(INPUT_GET, 'subpage', FILTER_SANITIZE_STRING));
 
-    <nav>
-        <a href="shop">Galerie</a>
-        <a href="help">Hilfe</a>
-        <a href="rss">RSS Feed</a>
-        <a href="admin">Administration</a>
-        <?php
-        if( TRUE ) {
-        ?>
-            <a><?php //TODO insert username, clickable for profile settings? ?></a>
-            <a>Logout</a>
-        <?php
-        } else { //nicht angemeldet
-        ?>
-            <a>Registrieren</a>
-            <a>Login</a>
-        <?php } ?>
-    </nav>
-
-    <main>
-        <?php
-        $subpage = trim(filter_input(INPUT_GET, 'subpage', FILTER_SANITIZE_STRING));
-
-        if(!empty( $subpage ) && file_exists( "../subpages/$subpage.php" ))
-            { include( "../subpages/$subpage.php" ); }
-        else
-            { include( "../subpages/shop.php" ); }
-        ?>
-    </main>
+            if(!empty( $subpage ) && file_exists( "../subpages/$subpage.php" ))
+                { include( "../subpages/$subpage.php" ); }
+            else
+                { include( "../subpages/shop.php" ); }
+            ?>
+            </main>
+        </div>
+    </div>
 
 <!-- TODO perhaps exclude these if already logged in -->
     <div id="dialog_register" title="Registrieren">
--- subpages/navBar.php
+++ subpages/navBar.php
@@ -1,46 +1,46 @@
 <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
         <a class="navbar-brand" href=".">
-            <img class="fh_logo" src="./img/logo-300x160_0.png" alt="Startseite">
+            <img class="fh_logo" src="./assets/FHLogo.png" alt="Startseite">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
-            <li class="nav-item <?php if ($_GET['page'] == "home") {?>active<?php }?>">
-                <a class="nav-link" href="?page=home">Home<?php
-                    if (isset($_GET['page']) && $_GET['page'] === "home") {
+            <li class="nav-item <?php if ($_GET['subpage'] == "shop") {?>active<?php }?>">
+                <a class="nav-link" href="shop">Galerie<?php
+                    if (isset($_GET['subpage']) && $_GET['subpage'] === "shop") {
                     ?><span class="sr-only"> (current)</span><?php
                     } ?>
                 </a>
             </li>            
-            <li class="nav-item <?php if ($_GET['page'] == "formular") {?>active<?php }?>">
-                <a class="nav-link" href="?page=<?php $pagename = "formular"; echo $pagename; ?>">Formular<?php
-                    if (isset($_GET['page']) && $_GET['page'] === "formular") {
+            <li class="nav-item <?php if ($_GET['subpage'] == "help") {?>active<?php }?>">
+                <a class="nav-link" href="help">Hilfe<?php
+                    if (isset($_GET['subpage']) && $_GET['subpage'] === "help") {
                       ?><span class="sr-only"> (current)</span><?php
                     }?>
                 </a>
             </li>
-            <li class="nav-item <?php if ($_GET['page'] == "memory") {?>active<?php }?>">
-                <a class="nav-link" href="?page=<?php $pagename = "memory"; echo $pagename; ?>">Memory<?php
-                    if (isset($_GET['page']) && $_GET['page'] === "memory") {
+            <li class="nav-item <?php if ($_GET['subpage'] == "rss") {?>active<?php }?>">
+                <a class="nav-link" href="rss">RSS Feed<?php
+                    if (isset($_GET['subpage']) && $_GET['subpage'] === "rss") {
                       ?><span class="sr-only"> (current)</span><?php
                     }?>
                 </a>
             </li>
-            <li class="nav-item <?php if ($_GET['page'] == "imgUpload") {?>active<?php }?>">
-                <a class="nav-link" href="?page=<?php $pagename = "imgUpload"; echo $pagename; ?>">Upload<?php
-                    if (isset($_GET['page']) && $_GET['page'] === "imgUpload") {
+            <li class="nav-item <?php if ($_GET['subpage'] == "admin") {?>active<?php }?>">
+                <a class="nav-link" href="admin">Administration<?php
+                    if (isset($_GET['subpage']) && $_GET['subpage'] === "admin") {
                       ?><span class="sr-only"> (current)</span><?php
                     }?>
                 </a>
             </li>
                 <?php if(empty($_SESSION["username"])) { 
-                  include("./sitecontent/loginNavigation.php");
+                  include("../subpages/loginNavigation.php");
                 }
                 else if(!empty($_SESSION["username"]))
                 {
-                  include("./sitecontent/logoutNavigation.php");
+                  include("../subpages/logoutNavigation.php");
                 }
                 ?>
             <!--
