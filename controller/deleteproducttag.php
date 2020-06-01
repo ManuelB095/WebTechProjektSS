54fbc10e99e4c2e81f30bfa3aa0225ea20307827
LukasReinhartGraf <if19b019@technikum-wien.at>
2020-06-01 10:31:55 +0200

    delete tag (untested)

---
+++ controller/deleteproducttag.php
@@ -0,0 +1,15 @@
+<?php
+
+// retrieve sanitised inputs
+$input = [
+    'tid' => filter_input(INPUT_POST, 'tid', FILTER_SANITIZE_NUMBER_INT),
+    'pid' => filter_input(INPUT_POST, 'pid', FILTER_SANITIZE_NUMBER_INT),
+];
+
+// From former "UserControl": make sure there are no unknown inputs?
+
+$db = new DB("DELETE FROM producttags WHERE tid = {$input['tid']} AND pid = {$input['pid']};");
+$db->Execute();
+
+//TODO error handling
+
