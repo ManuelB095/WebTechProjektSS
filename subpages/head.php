f705591686d67fdeb0d3ed76391f495cf6418d29
LukasReinhartGraf <if19b019@technikum-wien.at>
2020-06-01 13:35:41 +0200

    fix head order

--- subpages/head.php
+++ subpages/head.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" type=text/css href="css/bootstrap/bootstrap.min.css">
-    <script src="js/bootstrap/bootstrap.min.js"></script>
-
     <!-- JQuery -->
     <script src="js/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" type=text/css href="js/jquery-ui-1.12.1/jquery-ui.min.css" />
     <script src="js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type=text/css href="css/bootstrap/bootstrap.min.css">
+    <script src="js/bootstrap/bootstrap.min.js"></script>
 
     <!-- Custom -->
     <link rel="stylesheet" type=text/css href="css/CustomShop.css" />
