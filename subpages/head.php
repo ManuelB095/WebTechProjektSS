e7a1e05cc11e5970825531337bd3e581598d8783
LukasReinhartGraf <if19b019@technikum-wien.at>
2020-06-01 13:32:38 +0200

    no more hotlinking, website can be tested offline again

--- subpages/head.php
+++ subpages/head.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    
-    <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type=text/css href="js/jquery-ui-1.12.1/jquery-ui.min.css" />
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type=text/css href="css/bootstrap/bootstrap.min.css">
+    <script src="js/bootstrap/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" type=text/css href="assets/BasicModal.css" />
-    <link rel="stylesheet" type=text/css href="assets/CustomShop.css" />
+    <!-- JQuery -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" type=text/css href="js/jquery-ui-1.12.1/jquery-ui.min.css" />
+    <script src="js/jquery-ui-1.12.1/jquery-ui.min.js"></script>
+
+    <!-- Custom -->
+    <link rel="stylesheet" type=text/css href="css/CustomShop.css" />
     <link rel="stylesheet" type=text/css href="css/Navigation.css" />
     <link rel="shortcut icon" href="assets/fhtw-16x16.ico" >
     <script src="js/index.js"></script>
